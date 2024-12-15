3fe8d4ad53997fb14f7cb1207dce6e3a071a9a02
DutkinTE <dutkinte@gmail.com>
2024-12-16 01:42:04 +0300

    reverse cursor

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,28 @@
 const cursorOuter = document.createElement('div');
-  const cursorInner = document.createElement('div');
-  cursorOuter.classList.add('cursor-outer');
-  cursorInner.classList.add('cursor-inner');
-  document.body.appendChild(cursorOuter);
-  document.body.appendChild(cursorInner);
+const cursorInner = document.createElement('div');
+cursorOuter.classList.add('cursor-outer');
+cursorInner.classList.add('cursor-inner');
+document.body.appendChild(cursorOuter);
+document.body.appendChild(cursorInner);
 
-  // Отслеживаем движение мыши
-  document.addEventListener('mousemove', (e) => {
-    const { clientX, clientY } = e;
+document.addEventListener('mousemove', (e) => {
+  const { clientX, clientY } = e;
 
-    // Двигаем внешний круг
-    cursorOuter.style.transform = `translate(${clientX - 25}px, ${clientY - 25}px)`;
+  cursorOuter.style.transform = `translate(${clientX - 25}px, ${clientY - 25}px)`;
 
-    // Двигаем внутреннюю точку
-    cursorInner.style.transform = `translate(${clientX - 4.75}px, ${clientY - 4.75}px)`;
+  cursorInner.style.transform = `translate(${clientX - 4.75}px, ${clientY - 4.75}px)`;
+});
+
+
+const invertTargets = document.querySelectorAll('.invert-cursor');
+
+invertTargets.forEach((target) => {
+  target.addEventListener('mouseenter', () => {
+    cursorOuter.classList.add('invert');
+    cursorInner.classList.add('invert');
   });
+  target.addEventListener('mouseleave', () => {
+    cursorOuter.classList.remove('invert');
+    cursorInner.classList.remove('invert');
+  });
+});
