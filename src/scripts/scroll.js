1aa58f11eacbce5dc9782158c35d3db118ac1ea7
DutkinTE <dutkinte@gmail.com>
2025-01-20 02:42:47 +0300

    update

--- src/scripts/scroll.js
+++ src/scripts/scroll.js
@@ -2,41 +2,77 @@ let lastScrollY = 0;
 let ticking = false;
 
 if (window.scrollY > 750) {
-    document.getElementById('main-header').style.maxWidth = `1440px`;
+  document.getElementById('main-header').style.maxWidth = `1440px`;
 }
 
 function handleScroll() {
   const currentScrollY = window.scrollY;
 
   if (currentScrollY > lastScrollY) {
-    if (window.scrollY > 300 && window.scrollY < document.getElementById('who').offsetTop) {
+    if (window.scrollY > 300 && window.scrollY < 890) {
       document.getElementById('main-header').style.maxWidth = `1440px`;
       document.getElementById('who-title').style.color = `#f5f5f5`;
       document.querySelectorAll('.who-content1').forEach(element => {
         element.style.color = `#f5f5f5`;
       });
     }
-    else if (window.scrollY > document.getElementById('who').offsetTop + 50 && window.scrollY < document.getElementById('who').offsetTop + 400) {
+    else if (window.scrollY > 1190 && window.scrollY < 1940) {
       document.getElementById('who-title2').style.color = `#f5f5f5`;
       document.getElementById('who-title').style.color = `#5B5B5B`;
       document.querySelectorAll('.who-content1').forEach(element => {
         element.style.color = `#5B5B5B`;
       });
     }
-    else if (window.scrollY > document.getElementById('who').offsetTop + 450) {
+    else if (window.scrollY > 1940) {
       document.getElementById('skills').style.color = `#f5f5f5`;
       document.getElementById('who-title2').style.color = `#5B5B5B`;
     }
-  } else if (currentScrollY < lastScrollY) {
+  }
+  else if (currentScrollY < lastScrollY) {
     if (window.scrollY < document.getElementById('who').offsetTop) {
-
       document.getElementById('main-header').style.maxWidth = `100%`;
+      document.getElementById('who-title').style.color = `#5B5B5B`;
+      document.querySelectorAll('.who-content1').forEach(element => {
+        element.style.color = `#5B5B5B`;
+      });
+    }
+    if (window.scrollY > 300 && window.scrollY < 890) {
+      document.getElementById('who-title').style.color = `#f5f5f5`;
+      document.getElementById('who-title2').style.color = `#5B5B5B`;
+      document.querySelectorAll('.who-content1').forEach(element => {
+        element.style.color = `#f5f5f5`;
+      });
+    }
+    else if (window.scrollY > 1190 && window.scrollY < 1940) {
+      document.getElementById('who-title2').style.color = `#f5f5f5`;
+      document.getElementById('skills').style.color = `#5B5B5B`;
+      document.querySelectorAll('.who-content1').forEach(element => {
+        element.style.color = `#5B5B5B`;
+      });
+    }
+    else if (window.scrollY > 1940) {
+      document.getElementById('skills').style.color = `#f5f5f5`;
+      document.getElementById('who-title2').style.color = `#5B5B5B`;
     }
 
   }
 
   lastScrollY = currentScrollY;
   ticking = false;
+}
+
+function animation(selector) {
+  const elements = document.querySelectorAll(selector);
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  }, { threshold: 0.5 });
+
+  elements.forEach(element => observer.observe(element));
 }
 
 window.addEventListener('scroll', () => {
