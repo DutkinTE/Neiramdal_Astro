31fd368c66488f0b84f84ef73bbe89b86bccb95f
DutkinTE <dutkinte@gmail.com>
2025-01-31 14:10:34 +0300

    update

---
+++ src/scripts/nav.js
@@ -0,0 +1,23 @@
+document.getElementById("nav-who-we-are").addEventListener('click', () => {
+    document.getElementById('who').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+document.getElementById("nav-skills").addEventListener('click', () => {
+    document.getElementById('skills').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+document.getElementById("nav-projects").addEventListener('click', () => {
+    document.getElementById('projects').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+document.getElementById("nav-contacts").addEventListener('click', () => {
+    document.getElementById('footer').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
