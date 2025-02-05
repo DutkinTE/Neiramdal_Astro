ca1d64c5ed012fb0a6e8edf97afc8caaca90474c
DutkinTE <dutkinte@gmail.com>
2025-02-05 23:40:22 +0300

    update

--- src/scripts/sprites.js
+++ src/scripts/sprites.js
@@ -10,4 +10,4 @@ function animateSprite() {
     sprite.src = frames[frameIndex]; // Меняем изображение
 }
 
-setInterval(animateSprite, 20); // Обновляем каждые 200 мс
+setInterval(animateSprite, 33); // Обновляем каждые 200 мс
