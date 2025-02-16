f9cfba47af929b6641f76a1b10a2029f8b3c8f50
DutkinTE <dutkinte@gmail.com>
2025-02-17 00:01:23 +0300

    update

--- src/scripts/three.js
+++ src/scripts/three.js
@@ -19,8 +19,6 @@ requestAnimationFrame(() => {
     renderer.setSize(window.innerWidth, window.innerHeight);
     container.appendChild(renderer.domElement);
 
-    const ambientLight = new THREE.AmbientLight(0xffffff, 2);
-    scene.add(ambientLight);
 
     const directionalLight = new THREE.DirectionalLight(0xffffff, 3);
     directionalLight.position.set(5, 5, 5);
